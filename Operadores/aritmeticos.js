ed7b5b47715e78c28f6c298c84d41e0a5debe26d
magnzg <mariaaliceferreiragonzaga@gmail.com>
2025-01-29 08:18:59 -0300

    Initial commit

---
+++ Operadores/aritmeticos.js
@@ -0,0 +1,27 @@
+//console.log("Olá, Mundo!")
+
+let nome = "Juliana"
+var nomes = "Juliana"
+
+//CTRL + ; -> Comentarios
+console.log(nome)
+
+let idade = 24
+console.log("Minha idade é", idade, "anos")
+//CONCATENAÇÃO = juntar texto de valor 
+
+//OPERAÇÕES MATEMÁTICAS
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b //o que sobra da divisão
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
