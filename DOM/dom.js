3be19f4590c80597145c9c1d8cf01ff5a68c98f4
magnzg <mariaaliceferreiragonzaga@gmail.com>
2025-02-26 10:45:51 -0300

    lista13

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,23 +1,22 @@
-// ARQUIVO DOM.JS
-let imagem = document.getElementById("Imagem")
+let imagem = document.getElementById("imagem")
 
-imagem.addEventListener("mouseover", function() {
-    imagem.setAttribute("src", "vit.jpg")
-})
-imagem.addEventListener("mouseout", function() {
-    imagem.setAttribute("src", "ninha.jpg")
-})
-//////////////////////////////////////////////////////////
+// imagem.addEventListener("mouseover", function() {
+//     imagem.setAttribute("src", "jinx2.jpg")
+// })
+// imagem.addEventListener("mouseout", function (){
+//     imagem.setAttribute("src", "jinx1.jpg")
+// })
+
 let lista = document.getElementById("lista")
 let adicionar = document.getElementById("adicionar")
-let remover = document.getElementById("remover")
+let remover =document.getElementById("remover")
 
-adicionar.addEventListener("click", function() {
+adicionar.addEventListener("click", function (){
     let novoItem = document.createElement("li")
     novoItem.innerText = "Novo Item"
     lista.appendChild(novoItem)
 })
-remover.addEventListener("click", function () {
+remover.addEventListener("click", function (){
     if (lista.lastChild) {
         lista.removeChild(lista.lastChild)
     }
